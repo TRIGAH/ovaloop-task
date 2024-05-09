f5424055a8adc43cbcb9600bbb2bea0a5946288e
Trigah <ijenrandy2@gmail.com>
2024-05-09 17:50:59 +0100

    Update requirements

--- product/models.py
+++ product/models.py
@@ -24,6 +24,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ('-created_at',)
+
     def __str__(self):
          return f"{self.name}"
     
